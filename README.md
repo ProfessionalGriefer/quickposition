575c88fbad37d7aed5634504c08cc27fed28ca4e
Vincent Nahn <vincentnahn@gmail.com>
2024-10-22 19:28:15 +0200

    It works but some polishing is still needed

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quick Position
+
+Author: Vincent Nahn
--- quickposition.sh
+++ quickposition.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
-# Welcome Screen
-echo "Welcome to QuickPosition."
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+LEFT="${SCRIPT_DIR}/iPad Left"
+RIGHT="${SCRIPT_DIR}/iPad Right"
+
+function openDisplaySettings() {
+    open /System/Library/PreferencePanes/Displays.prefPane
+}
+function printSettings() {
+    /opt/homebrew/bin/displayplacer list | tail -n 1 | sed 's/^displayplacer /\/opt\/homebrew\/bin\/displayplacer /' | sed '1i\
+#!/bin/bash
+    '
+}
+
 
 # Confirm
-read -p "Are you sure? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
-fi
+function confirm() {
+    read -p "$1" -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    fi
+}
 
+# Welcome Screen
+echo "Welcome to QuickPosition."
+confirm "The display settings are going to open. (y/n) "
 
-# Open Display settings
-open /System/Library/PreferencePanes/Displays.prefPane
+openDisplaySettings
+confirm "Did you position your iPad to the LEFT? (y/n) "
+printSettings > "${LEFT}/Contents/MacOS/iPadLeft"
 
-# Set for left side
-/opt/homebrew/bin/displayplacer list
+openDisplaySettings
+confirm "Did you position your iPad to the RIGHT? (y/n) "
+printSettings > "${RIGHT}/Contents/MacOS/iPadRight"
 
-# Open Display settings
-open /System/Library/PreferencePanes/Displays.prefPane
+# # Copy two folders in /Applications
 
-# Set for right side
-/opt/homebrew/bin/displayplacer list
+mkdir -p "/Applications/iPad Left.app"
+mkdir -p "/Applications/iPad Right.app"
+cp -R "$LEFT" "/Applications/iPad Left.app"
+cp -R "$RIGHT" "/Applications/iPad Right.app"
 
-# Copy two folders in /Applications
+# ls "${SCRIPT_DIR}/iPad Left.app"
 
-# Write displayplacer command string into iPadLeft and iPadRight
-
-# Exits
+# # Exits
+echo "Thank you for using Quick Position!"
