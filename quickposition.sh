f1af3d43784249a2b1e8bd9e1bb392c7f6f90e20
Vincent Nahn <vincentnahn@gmail.com>
2024-10-22 19:50:40 +0200

    Fixed Bug

--- quickposition.sh
+++ quickposition.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+###############################################################################
+#  ________  ___  ___  ___  ________  ___  __                                 #
+# |\   __  \|\  \|\  \|\  \|\   ____\|\  \|\  \                               #
+# \ \  \|\  \ \  \\\  \ \  \ \  \___|\ \  \/  /|_                             #
+#  \ \  \\\  \ \  \\\  \ \  \ \  \    \ \   ___  \                            #
+#   \ \  \\\  \ \  \\\  \ \  \ \  \____\ \  \\ \  \                           #
+#    \ \_____  \ \_______\ \__\ \_______\ \__\\ \__\                          #
+#     \|___| \__\|_______|\|__|\|_______|\|__| \|__|                          #
+#           \|__|                                                             #
+#                                                                             #
+#  ________  ________  ________  ___  _________  ___  ________  ________      #
+# |\   __  \|\   __  \|\   ____\|\  \|\___   ___\\  \|\   __  \|\   ___  \    #
+# \ \  \|\  \ \  \|\  \ \  \___|\ \  \|___ \  \_\ \  \ \  \|\  \ \  \\ \  \   #
+#  \ \   ____\ \  \\\  \ \_____  \ \  \   \ \  \ \ \  \ \  \\\  \ \  \\ \  \  #
+#   \ \  \___|\ \  \\\  \|____|\  \ \  \   \ \  \ \ \  \ \  \\\  \ \  \\ \  \ #
+#    \ \__\    \ \_______\____\_\  \ \__\   \ \__\ \ \__\ \_______\ \__\\ \__\#
+#     \|__|     \|_______|\_________\|__|    \|__|  \|__|\|_______|\|__| \|__|#
+#                        \|_________|                                         #
+###############################################################################
+
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-LEFT="${SCRIPT_DIR}/iPad Left"
-RIGHT="${SCRIPT_DIR}/iPad Right"
+LEFT="iPad Left.app"
+RIGHT="iPad Right.app"
 
 function openDisplaySettings() {
     open /System/Library/PreferencePanes/Displays.prefPane
@@ -26,24 +47,25 @@ function confirm() {
 
 # Welcome Screen
 echo "Welcome to QuickPosition."
-confirm "The display settings are going to open. (y/n) "
+confirm "The display settings are going to open. First, position your iPad to the left side of your MacBook (y/n) "
 
+# Left side
 openDisplaySettings
 confirm "Did you position your iPad to the LEFT? (y/n) "
-printSettings > "${LEFT}/Contents/MacOS/iPadLeft"
+printSettings > "${SCRIPT_DIR}/${LEFT}/Contents/MacOS/iPadLeft"
 
+# Right side
 openDisplaySettings
 confirm "Did you position your iPad to the RIGHT? (y/n) "
-printSettings > "${RIGHT}/Contents/MacOS/iPadRight"
+printSettings > "${SCRIPT_DIR}/${RIGHT}/Contents/MacOS/iPadRight"
 
-# # Copy two folders in /Applications
+# Copy the two folders into /Applications
 
-mkdir -p "/Applications/iPad Left.app"
-mkdir -p "/Applications/iPad Right.app"
-cp -R "$LEFT" "/Applications/iPad Left.app"
-cp -R "$RIGHT" "/Applications/iPad Right.app"
+rm -rf "/Applications/${LEFT}"
+rm -rf "/Applications/${RIGHT}"
+cp -R "$LEFT" "/Applications"
+cp -R "$RIGHT" "/Applications"
 
-# ls "${SCRIPT_DIR}/iPad Left.app"
 
-# # Exits
+# Exits
 echo "Thank you for using Quick Position!"
